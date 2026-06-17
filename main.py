361e67404a517a03402966365d7020d6fc43bd2e
Herbert dos Reis <herbertfmreis@gmail.com>
2026-06-17 11:15:04 -0300

    feat: add config da tela

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
 import pygame
+from random import randint
+from os.path import join
 
 pygame.init()
 
-tela = pygame.display.set_mode((800,600))
+# configurações base
+largura,altura = 800,600
+display_surface = pygame.display.set_mode((largura,altura))
+name_game = pygame.display.set_caption(('Rei do Programa'))
+clock = pygame.time.Clock()
+
+#script do 
+
+rodando = True
+while rodando:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+
+    display_surface.fill ('darkgray')
+   
+    pygame.display.update()
+    
+
+pygame.quit()

445d35ff7faf521c785be3fe36d9ba2b8b96492f
Herbert dos Reis <herbertfmreis@gmail.com>
2026-06-20 19:17:25 -0300

    feat:display score

---
+++ interface.py
@@ -0,0 +1,42 @@
+import pygame
+from os.path import join
+
+pygame.init()
+
+#setup display temporario
+largura,altura = 800,600
+tela = pygame.display.set_mode((largura,altura))
+game_name = pygame.display.set_caption('Rei do Programa')
+
+#criando texto
+def score_tela():
+    tempo_atual = pygame.time.get_ticks()//100
+    fonte = pygame.font.Font(join('Assets', 'Pixeboy.ttf'),50)
+    text_surf = fonte.render(str(tempo_atual), False,(240,240,240))
+    text_rect = text_surf.get_frect(midbottom = (largura/2,altura-50))
+    tela.blit(text_surf,text_rect)
+    pygame.draw.rect(tela, 'white',text_rect.inflate(20,15).move(0,-12),5,10)
+
+
+
+
+#loop main
+rodando=True
+while rodando:
+    #eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+        
+    #update
+    
+
+    #draw
+    tela.fill('black')
+    score_tela()
+    
+
+    pygame.display.update()
+
+pygame.quit()    
+

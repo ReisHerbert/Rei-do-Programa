af4ebbb07bddced57ea8d4b14cbbd0c1b7675a84
Herbert dos Reis <herbertfmreis@gmail.com>
2026-06-20 15:25:14 -0300

    feat:projetil.png

--- jogador.py
+++ jogador.py
@@ -9,16 +9,16 @@ tela = pygame.display.set_mode((largura,altura))
 relogio = pygame.time.Clock()
 
 class Jogador(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load (join('player.png'))
+    def __init__(self, groups):
+        super().__init__(todos_sprites)
+        self.image = pygame.image.load (join('Assets','Jogador','Dai-Recovered4.png'))
         self.rect = self.image.get_frect(center = (largura/2,altura/2))
 
         #cooldown
 
         self.pode_atirar = True
         self.tempo_tiro = 0
-        self.duracao_cooldown = 400
+        self.duracao_cooldown = 200
     
     def temporizador_tiro(self):
         if not self.pode_atirar:
@@ -37,8 +37,7 @@ class Jogador(pygame.sprite.Sprite):
         if keys[pygame.K_w]:
             self.rect.y -= 5
         recent_keys = pygame.key.get_just_pressed()
-        if recent_keys[pygame.K_RIGHT] and self.can_shoot:
-            print("fire shoot")
+        if recent_keys[pygame.K_SPACE] and self.pode_atirar:
             Tiro(self.rect.midtop,groups=(todos_sprites,tiro_sprites))
             self.pode_atirar = False
             self.tempo_tiro = pygame.time.get_ticks() 
@@ -48,14 +47,18 @@ class Jogador(pygame.sprite.Sprite):
 class Tiro(pygame.sprite.Sprite):
     def __init__(self,pos,groups):
         super().__init__(groups)
-        self.image = pygame.image.load(join("player.png"))
+        self.image = pygame.image.load(join('Assets','Jogador','Projetil.png'))
         self.rect = self.image.get_rect(midbottom = pos)
+        self.velociade = 5
     def update(self):
-        self.rect.centery-=400
+        self.rect.centery-= self.velociade
+        if self.rect.bottom <0:
+            self.kill()
 
 
 #sprites
 todos_sprites = pygame.sprite.Group()
+jogador = Jogador(todos_sprites)
 tiro_sprites = pygame.sprite.Group()
 inimigos_sprites = pygame.sprite.Group()
 
@@ -69,11 +72,17 @@ while rodando:
     for tiro in tiro_sprites:
         pygame.sprite.spritecollide(tiro,inimigos_sprites, True)
 
+    #draws
+
     tela.fill('darkgrey')
     todos_sprites.draw(tela)
     tiro_sprites.draw(tela)
 
+    #update
     pygame.display.update()
+    todos_sprites.update()
+    tiro_sprites.update()
+
 
 pygame.quit()
 

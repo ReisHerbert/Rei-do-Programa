521222650a2e09bc1a81bfe20d6d8170fb3b139d
Herbert dos Reis <herbertfmreis@gmail.com>
2026-06-20 17:20:14 -0300

    feat:direcao do jogador e do tiro(4)

--- jogador.py
+++ jogador.py
@@ -11,14 +11,34 @@ relogio = pygame.time.Clock()
 class Jogador(pygame.sprite.Sprite):
     def __init__(self, groups):
         super().__init__(todos_sprites)
-        self.image = pygame.image.load (join('Assets','Jogador','Dai-Recovered4.png'))
+
+        #sprites
+        self.sprites = {
+        "right":pygame.image.load(join('Assets','Jogador','player_right.png')).convert_alpha(),
+        "left": pygame.image.load(join('Assets','Jogador','player_left.png')).convert_alpha(),
+        "up":pygame.image.load(join('Assets','Jogador','player_up.png')).convert_alpha(),
+        "down" :pygame.image.load(join('Assets','Jogador','player_down.png')).convert_alpha(),
+        }
+        
+        #direcao
+        self.direcao = "down"     
+        self.image = self.sprites[self.direcao]
         self.rect = self.image.get_frect(center = (largura/2,altura/2))
 
-        #cooldown
+        #movimento
+        self.velocidade = 3
 
+        #tiro
+        
         self.pode_atirar = True
         self.tempo_tiro = 0
         self.duracao_cooldown = 200
+        self.direcao_tiro = {
+        "up": pygame.Vector2(0, -1),
+        "down": pygame.Vector2(0, 1),
+        "left": pygame.Vector2(-1, 0),
+        "right": pygame.Vector2(1, 0),
+}
     
     def temporizador_tiro(self):
         if not self.pode_atirar:
@@ -28,34 +48,58 @@ class Jogador(pygame.sprite.Sprite):
 
     def update(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_a]:
-            self.rect.x -= 5
-        if keys[pygame.K_d]: 
-            self.rect.x += 5
-        if keys[pygame.K_s]:
-            self.rect.y += 5
-        if keys[pygame.K_w]:
-            self.rect.y -= 5
+        moving = False
+        if keys[pygame.K_d]:
+            self.rect.x += self.velocidade
+            self.direcao = "right"
+            moving = True
+
+        elif keys[pygame.K_a]:
+            self.rect.x -= self.velocidade
+            self.direcao = "left"
+            moving = True
+
+        elif keys[pygame.K_w]:
+            self.rect.y -= self.velocidade
+            self.direcao = "up"
+            moving = True
+
+        elif keys[pygame.K_s]:
+            self.rect.y += self.velocidade
+            self.direcao = "down"
+            moving = True
+
+        #input do tiro
         recent_keys = pygame.key.get_just_pressed()
-        if recent_keys[pygame.K_SPACE] and self.pode_atirar:
-            Tiro(self.rect.midtop,groups=(todos_sprites,tiro_sprites))
+        if recent_keys[pygame.K_k] and self.pode_atirar:
+            Tiro(
+            self.rect.center,
+            self.direcao_tiro[self.direcao],
+            groups=(todos_sprites,tiro_sprites))
+
             self.pode_atirar = False
             self.tempo_tiro = pygame.time.get_ticks() 
         self.temporizador_tiro()
+
 class Tiro(pygame.sprite.Sprite):
-    def __init__(self,pos,groups):
+    def __init__(self,pos,direcao,groups):
         super().__init__(groups)
+
         self.image = pygame.image.load(join('Assets','Jogador','Projetil.png'))
-        self.rect = self.image.get_rect(midbottom = pos)
-        self.velociade = 5
+        self.rect = self.image.get_rect(center = pos)
+        self.direcao_tiro = direcao
+        self.velocidade = 5
+
     def update(self):
-        self.rect.centery-= self.velociade
-        if self.rect.bottom <0:
+        self.rect.x += self.direcao_tiro.x * self.velocidade
+        self.rect.y += self.direcao_tiro.y * self.velocidade
+        if not pygame.display.get_surface().get_rect().colliderect(self.rect):
             self.kill()
+
 def colisao():
     colisao_jogador = pygame.sprite.spritecollide(jogador, inimigos_sprites, True)
     if colisao_jogador:
-        print()
+        pass
     for tiro in tiro_sprites:
         sprites_colididos = pygame.sprite.spritecollide(tiro,inimigos_sprites,True)
         if sprites_colididos:
@@ -83,6 +127,7 @@ while rodando:
     tela.fill('darkgrey')
     todos_sprites.draw(tela)
     tiro_sprites.draw(tela)
+    colisao()
 
     #update
     pygame.display.update()

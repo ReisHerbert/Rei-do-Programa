0a45a22edd9447ef92c37e2889efc42bfa44a0f7
Herbert dos Reis <herbertfmreis@gmail.com>
2026-06-21 16:20:52 -0300

    feat: finalizacao da interface e setup cores in configs

--- configs.py
+++ configs.py
@@ -4,3 +4,9 @@ from os.path import join
 largura,altura = 800,600
 name_game = "Rei do Programa"
 relogio = pygame.time.Clock
+
+Cores = {
+    "azul": (37, 99, 235),
+    "vermelho": (220, 38, 38),
+    "branco": (203, 213, 225),
+}
